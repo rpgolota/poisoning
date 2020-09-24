484f0f55bbc7b577ae39f8274aa3138262fdcd71
Roman Golota <rpgolota@gmail.com>
2020-09-23 20:48:10 -0700

    added classifier and loss function

--- poisoning/xiao2018/algorithm.py
+++ poisoning/xiao2018/algorithm.py
@@ -1,23 +1,34 @@
-import sklearn
+import numpy as np
+import sklearn.linear_model
 
 # sklearn.linear_model.Ridge
 # sklearn.linear_model.Lasso
 # sklearn.linear_model.ElasticNet
 
 def classifier(weights, biases, x):
-    pass
+    """Linear classifier 
 
-def loss(classified, y):
+    Args:
+        weights (column vector): self-explanatory
+        biases (column vector): self-explanatory
+        x (column vector): column vector of features of data point
+
+    Returns:
+        column vector: weighted data point
+    """
+    return np.transpose(weights)*x - biases
+
+def loss(classified, expected):
     """Quadratic loss function
 
     Args:
-        classified (number): optput of linear classifier function
-        y (number): other input of dataset
+        classified (column vector): optput of linear classifier function
+        y (column vector): expected input
 
     Returns:
-        number: loss number
+        column vector: loss between classified result and expected
     """
-    return (1/2) * (classified - y)**2
+    return (1/2) * np.square(classified - y)
 
 def xiao2018():
     return 'Not implemented'
