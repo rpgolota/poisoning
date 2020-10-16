72c3e6c939c4e8b4aa16f4f83a090a7d950c9929
Roman Golota <rpgolota@gmail.com>
2020-10-16 11:32:42 -0700

    parametrized tests, still need to parametrize inputs

--- tests/Gradient_test.py
+++ tests/Gradient_test.py
@@ -1,37 +1,31 @@
 from poisoning.xiao2018.gradient import r_term, subgradient
+from poisoning.xiao2018.equations import equation7
 import numpy as np
 import pytest
+
+class Test_r_term_fail:
+    
+    okay_weights = np.array([11, 9.2, -1.1, 3.2, -2.3, 0.2, 0.0, -2.3, 0, 23])
+
+    @pytest.mark.parametrize('param', [{'this':3}, {'test123':332}, {'other':'haha'}, {'thing':'t'}])
+    def test_parameters(self, param):
+        with pytest.raises(TypeError) as info:
+            r_term(Test_r_term_fail.okay_weights, **param)
+
+    @pytest.mark.parametrize('tp', ['notlasso', 3, 92, 'other', 9.2, True])
+    def test_type(self, tp):
+        with pytest.raises(TypeError) as info:
+            r_term(Test_r_term_fail.okay_weights, type=tp)
+
+    @pytest.mark.parametrize('value', [-2, 3])
+    def test_range(self, value):
+        with pytest.raises(TypeError) as info:
+            r_term(Test_r_term_fail.okay_weights, range_value=value)
 
 class Test_r_term:
     
     okay_weights = np.array([11, 9.2, -1.1, 3.2, -2.3, 0.2, 0.0, -2.3, 0, 23])
     
-    def test_invalid_parameters(self):
-        
-        with pytest.raises(TypeError) as info:
-            r_term(Test_r_term.okay_weights, this=3)
-    
-        with pytest.raises(TypeError) as info:
-            r_term(Test_r_term.okay_weights, other='haha')
-
-    def test_invalid_type(self):
-        
-        with pytest.raises(TypeError) as info:
-            r_term(Test_r_term.okay_weights, type='not allowed')
-    
-        with pytest.raises(TypeError) as info:
-            r_term(Test_r_term.okay_weights, other='elastic net')
-
-    def test_invalid_range(self):
-        
-        with pytest.raises(TypeError) as info:
-            r_term(Test_r_term.okay_weights, range_value=-2)
-    
-        with pytest.raises(TypeError) as info:
-            r_term(Test_r_term.okay_weights, range_value=3)
-            
-        r_term(Test_r_term.okay_weights, range_value=1)
-        r_term(Test_r_term.okay_weights, range_value=-1)
     
     def test_lasso(self):
         assert all([a == b for a, b in zip(subgradient(Test_r_term.okay_weights), r_term(Test_r_term.okay_weights, type='lasso'))])
@@ -42,9 +36,15 @@ class Test_r_term:
     def test_elastic(self):
         assert all([a == b for a, b in zip((0.5 * subgradient(Test_r_term.okay_weights) + (1 - 0.5) * Test_r_term.okay_weights), r_term(Test_r_term.okay_weights, type='elastic'))])
     
+    @pytest.mark.parametrize('value', [1, -1])
+    def test_range(self, value):    
+        r_term(Test_r_term_fail.okay_weights, range_value=value)
+        
+    @pytest.mark.skip(reason='Not implemented')
     def test_elastic_rho(self):
         pass
-    
+
+
 class Test_subgradient:
     
     okay_weights = np.array([11, 9.2, -1.1, 3.2, -2.3, 0.2, 0.0, -2.3, 0, 23])
@@ -56,3 +56,40 @@ class Test_subgradient:
     def test_value_range_value(self):
         
         assert all([a == b for a, b in zip([1, 1, -1, 1, -1, 1, 0.3, -1, 0.3, 1], subgradient(Test_subgradient.okay_weights, Test_subgradient.okay_range_value))])
+
+    
+class Test_Equation_7_fail:
+    okay_X = [[1, 2], [3, 4]]
+    okay_Y = [1, 2]
+    okay_A = [3, 1]
+    okay_weights = [0.2, 1.3]
+    okay_biases = 0.1
+    okay_lambda = 0.23
+    
+    @pytest.mark.parametrize('param', [{'this':3}, {'test123':332}, {'other':'haha'}, {'thing':'t'}])
+    def test_parameters(self, param):
+        with pytest.raises(TypeError) as info:
+            equation7(Test_Equation_7_fail.okay_X, Test_Equation_7_fail.okay_Y, Test_Equation_7_fail.okay_A, Test_Equation_7_fail.okay_weights, Test_Equation_7_fail.okay_biases, **param)        
+
+    @pytest.mark.parametrize('tp', ['notlasso', 3, 92, 'other', 9.2, True])
+    def test_type(self, tp):
+        with pytest.raises(TypeError) as info:
+            equation7(Test_Equation_7_fail.okay_X, Test_Equation_7_fail.okay_Y, Test_Equation_7_fail.okay_A, Test_Equation_7_fail.okay_weights, Test_Equation_7_fail.okay_biases, type=tp)
+    
+    @pytest.mark.parametrize('addx,addy,adda,addw', [([[3,3]],[],[],[]),([],[3],[],[]),([],[],[3],[]),([],[],[],[3])])
+    def test_dimensions(self, addx,addy,adda,addw):
+        
+        with pytest.raises(ValueError) as info:
+            equation7(Test_Equation_7_fail.okay_X + addx, Test_Equation_7_fail.okay_Y + addy, Test_Equation_7_fail.okay_A +adda, Test_Equation_7_fail.okay_weights + addw, Test_Equation_7_fail.okay_biases)
+            
+class Test_Equation_7:
+    
+    okay_X = [[1, 2], [3, 4]]
+    okay_Y = [1, 2]
+    okay_A = [3, 1]
+    okay_weights = [0.2, 1.3]
+    okay_biases = 0.1
+    okay_lambda = 0.23
+    
+    def test_run(self):
+        equation7(Test_Equation_7.okay_X, Test_Equation_7.okay_Y, Test_Equation_7.okay_A, Test_Equation_7.okay_weights, Test_Equation_7.okay_biases)
--- tests/Linear_model_test.py
+++ tests/Linear_model_test.py
@@ -1,117 +1,41 @@
-from poisoning.xiao2018.equations import equation2, equation7
+from poisoning.xiao2018.equations import equation2
 import sklearn.linear_model as LM
 import pytest
+
+class Test_Equation_2_fail:
+    
+    okay_X = [[1, 2], [3, 4]]
+    okay_Y = [1, 2]
+    okay_lambda = 0.23
+    
+    @pytest.mark.parametrize('param', [{'this':3}, {'test123':332}, {'other':'haha'}, {'thing':'t'}])
+    def test_parameters(self, param):
+        with pytest.raises(TypeError) as info:
+            equation2(Test_Equation_2_fail.okay_X, Test_Equation_2_fail.okay_Y, **param)        
+    
+    @pytest.mark.parametrize('tp', ['notlasso', 3, 92, 'other', 9.2, True])
+    def test_type(self, tp):
+        with pytest.raises(TypeError) as info:
+            equation2(Test_Equation_2_fail.okay_X, Test_Equation_2_fail.okay_Y, type=tp)
+    
+    @pytest.mark.parametrize('addx,addy', [([],[3]),([[1,2]],[]),([[1,2]],[3, 4])])
+    def test_dimensions(self, addx, addy):
+        with pytest.raises(ValueError) as info:
+            equation2(Test_Equation_2_fail.okay_X + addx, Test_Equation_2_fail.okay_Y + addy)
 
 class Test_Equation_2:
     
     okay_X = [[1, 2], [3, 4]]
     okay_Y = [1, 2]
-    okay_lambda = 0.23
     
-    def test_invalid_parameters(self):
+    @pytest.mark.parametrize('alph', [1.0, 0.23])
+    @pytest.mark.parametrize('tp', ['lasso', 'ridge', 'elastic'])
+    def test_results(self, tp, alph):
+        res = equation2(Test_Equation_2.okay_X, Test_Equation_2.okay_Y, type=tp, alpha=alph)
+        alg = LM.Lasso(alpha=alph) if tp == 'lasso' else LM.Ridge(alpha=alph) if tp == 'ridge' else LM.ElasticNet(alpha=alph)
         
-        with pytest.raises(TypeError) as info:
-            equation2(Test_Equation_2.okay_X, Test_Equation_2.okay_Y, this=3)        
-    
-        with pytest.raises(TypeError) as info:
-            equation2(Test_Equation_2.okay_X, Test_Equation_2.okay_Y, other='haha')
-    
-    def test_invalid_type(self):
-        
-        with pytest.raises(TypeError) as info:
-            equation2(Test_Equation_2.okay_X, Test_Equation_2.okay_Y, type='not allowed')
-    
-        with pytest.raises(TypeError) as info:
-            equation2(Test_Equation_2.okay_X, Test_Equation_2.okay_Y, other='elastic net')
-            
-    def test_invalid_dimensions(self):
-        
-        with pytest.raises(ValueError) as info:
-            equation2(Test_Equation_2.okay_X, Test_Equation_2.okay_Y + [3])
-            
-    def test_lasso(self):
-        
-        res = equation2(Test_Equation_2.okay_X, Test_Equation_2.okay_Y, type='lasso')
-        accurate = LM.Lasso().fit(Test_Equation_2.okay_X, Test_Equation_2.okay_Y)
+        accurate = alg.fit(Test_Equation_2.okay_X, Test_Equation_2.okay_Y)
         accurate = (accurate.coef_, accurate.intercept_)
         
         assert all([r == a for (r, a) in zip(res[0], accurate[0])])
         assert res[1], accurate[1]
-        
-    def test_ridge(self):
-            
-        res = equation2(Test_Equation_2.okay_X, Test_Equation_2.okay_Y, type='ridge')
-        accurate = LM.Ridge().fit(Test_Equation_2.okay_X, Test_Equation_2.okay_Y)
-        accurate = (accurate.coef_, accurate.intercept_)
-        
-        assert all([r == a for (r, a) in zip(res[0], accurate[0])])
-        assert res[1], accurate[1]
-    
-    def test_elastic(self):
-            
-        res = equation2(Test_Equation_2.okay_X, Test_Equation_2.okay_Y, type='elastic')
-        accurate = LM.ElasticNet().fit(Test_Equation_2.okay_X, Test_Equation_2.okay_Y)
-        accurate = (accurate.coef_, accurate.intercept_)
-        
-        assert all([r == a for (r, a) in zip(res[0], accurate[0])])
-        assert res[1], accurate[1]
-        
-    def test_lasso_lambda(self):
-        
-        res = equation2(Test_Equation_2.okay_X, Test_Equation_2.okay_Y, type='lasso', alpha=Test_Equation_2.okay_lambda)
-        accurate = LM.Lasso(alpha=Test_Equation_2.okay_lambda).fit(Test_Equation_2.okay_X, Test_Equation_2.okay_Y)
-        accurate = (accurate.coef_, accurate.intercept_)
-        
-        assert all([r == a for (r, a) in zip(res[0], accurate[0])])
-        assert res[1], accurate[1]
-        
-    def test_ridge_lambda(self):
-            
-        res = equation2(Test_Equation_2.okay_X, Test_Equation_2.okay_Y, type='ridge', alpha=Test_Equation_2.okay_lambda)
-        accurate = LM.Ridge(alpha=Test_Equation_2.okay_lambda).fit(Test_Equation_2.okay_X, Test_Equation_2.okay_Y)
-        accurate = (accurate.coef_, accurate.intercept_)
-        
-        assert all([r == a for (r, a) in zip(res[0], accurate[0])])
-        assert res[1], accurate[1]
-    
-    def test_elastic_lambda(self):
-            
-        res = equation2(Test_Equation_2.okay_X, Test_Equation_2.okay_Y, type='elastic', alpha=Test_Equation_2.okay_lambda)
-        accurate = LM.ElasticNet(alpha=Test_Equation_2.okay_lambda).fit(Test_Equation_2.okay_X, Test_Equation_2.okay_Y)
-        accurate = (accurate.coef_, accurate.intercept_)
-        
-        assert all([r == a for (r, a) in zip(res[0], accurate[0])])
-        assert res[1], accurate[1]
-
-        
-class Test_Equation_7:
-    
-    okay_X = [[1, 2], [3, 4]]
-    okay_Y = [1, 2]
-    okay_weights = [0.2, 1.3]
-    okay_biases = 0.1
-    okay_lambda = 0.23
-    
-    def test_invalid_parameters(self):
-        
-        with pytest.raises(TypeError) as info:
-            equation7(Test_Equation_7.okay_X, Test_Equation_7.okay_Y, Test_Equation_7.okay_weights, Test_Equation_7.okay_biases, this=3)        
-    
-        with pytest.raises(TypeError) as info:
-            equation7(Test_Equation_7.okay_X, Test_Equation_7.okay_Y, Test_Equation_7.okay_weights, Test_Equation_7.okay_biases, other='haha')
-    
-    def test_invalid_type(self):
-        
-        with pytest.raises(TypeError) as info:
-            equation7(Test_Equation_7.okay_X, Test_Equation_7.okay_Y, Test_Equation_7.okay_weights, Test_Equation_7.okay_biases, type='not allowed')
-    
-        with pytest.raises(TypeError) as info:
-            equation7(Test_Equation_7.okay_X, Test_Equation_7.okay_Y, Test_Equation_7.okay_weights, Test_Equation_7.okay_biases, other='elastic net')
-            
-    def test_invalid_dimensions(self):
-        
-        with pytest.raises(ValueError) as info:
-            equation7(Test_Equation_7.okay_X, Test_Equation_7.okay_Y  + [3], Test_Equation_7.okay_weights, Test_Equation_7.okay_biases)
-            
-        with pytest.raises(ValueError) as info:
-            equation7(Test_Equation_7.okay_X, Test_Equation_7.okay_Y  + [3], Test_Equation_7.okay_weights + [3], Test_Equation_7.okay_biases)
