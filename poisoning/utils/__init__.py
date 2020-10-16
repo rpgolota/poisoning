1d8c325596d187e7d9978202c57a8cee33fb46c9
Roman Golota <rpgolota@gmail.com>
2020-10-16 01:20:59 -0700

    added logging capabilities, relative imports causing problems though"

---
+++ poisoning/utils/__init__.py
@@ -0,0 +1,3 @@
+from .logger import PoisonLogger
+
+__all__ = ('PoisonLogger')
---
+++ poisoning/utils/logger.py
@@ -0,0 +1,33 @@
+import logging
+
+class PoisonLogger:
+    
+    GENERAL_LOGGER = logging.getLogger('PoisonLog')
+    GENERAL_LOGGER.setLevel(logging.INFO)
+    GENERAL_HANDLER = logging.StreamHandler()
+    FILE_HANDLER = logging.FileHandler('poison.log')    
+    
+    GENERAL_FMT = ('[%(asctime)s.%(msecs)03d][%(name)s][%(levelname)-8s][%(filename)s][%(funcName)s][%(lineno)d] : %(message)s', '%Y/%m/%d][%H:%M:%S')
+    GENERAL_FOMATTER = logging.Formatter(*GENERAL_FMT)
+    
+    GENERAL_HANDLER.setFormatter(GENERAL_FOMATTER)
+    FILE_HANDLER.setFormatter(GENERAL_FOMATTER)
+    
+    GENERAL_LOGGER.addHandler(GENERAL_HANDLER)
+    GENERAL_LOGGER.addHandler(FILE_HANDLER)
+
+    debug = GENERAL_LOGGER.debug
+    info = GENERAL_LOGGER.info
+    warn = GENERAL_LOGGER.warn
+    error = GENERAL_LOGGER.error
+    critical = GENERAL_LOGGER.critical
+    exception = GENERAL_LOGGER.exception
+    
+    def disable_debug():
+        PoisonLogger.debug = PoisonLogger.nothing
+    
+    def enable_debug():
+        PoisonLogger.debug = PoisonLogger.GENERAL_LOGGER.debug
+    
+    def nothing(*args, **kwargs):
+        pass
