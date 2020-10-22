b145a777ea052530aae1a5c2eac51a3ff4db204e
Roman Golota <rpgolota@gmail.com>
2020-10-21 22:45:56 -0700

    added equation4 (gradient), and moved equation7 to gradient.py

--- poisoning/xiao2018/gradient.py
+++ poisoning/xiao2018/gradient.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from sys import float_info
 import sys, os
 
 # current workaround to support imports from top level for things above the scope of this package
@@ -75,3 +75,151 @@ def subgradient(weights, range_value=0.0):
     PoisonLogger.info(f'Got argument for range_value::{range_value}.')
     
     return np.array([-1 if i < 0 else 1 if i > 0 else range_value for i in weights])
+
+def equation7(X, Y, ax, ay, weights, biases, **kwargs):
+    
+    """Equation 7 in paper. Returns the partial derivatives of the weights and biases with respect to the attack point.
+
+    Parameters:
+        X: Data
+        Y: Results
+        ax: Attack point
+        ay: Attack point y value
+        weights: weights
+        biases: biases
+        Arguments:
+            type : (str) : type of algorithm to use ::: {'lasso', 'ridge', 'elastic'}
+                default => 'lasso'
+            alpha : (number) : alpha to be used in linear model by sklearn
+                default => 1.0
+            rho : (number) : rho used in sklearn for elastic net. Same as l1_ratio
+                default => 0.5
+                
+    Raises:
+        ValueError: Invalid X and Y dimensions
+        TypeError: Unknown parameters
+        TypeError: Invalid algorithm type
+
+    Returns:
+        Tuple: partial derivatives with respect to weights for (weights, biases)
+    """
+    
+    if kwargs:
+        PoisonLogger.info(f'Got arguments: {kwargs}')
+    
+    algorithm = kwargs.pop('type', 'lasso')
+    alph = kwargs.pop('alpha', 1.0)
+    rho = kwargs.pop('rho', 0.5)
+    
+    if kwargs:
+        raise TypeError('Unknown parameters: ' + ', '.join(kwargs.keys()))
+    
+    X = np.array(X)
+    Y = np.array(Y)
+    ax = np.array(ax)
+    weights = np.array(weights)
+    
+    if (X.shape[0] != Y.shape[0] or X.shape[1] != ax.shape[0] or X.shape[1] != weights.shape[0]):
+        raise ValueError('X and Y must have the same dimensions. ax and weights must have same dimensions. X second dimension must equal ax dimension.')
+    
+    n = X.shape[1]
+    PoisonLogger.info(f'Got second dimension of X:{n}.')
+    
+    if algorithm == 'lasso':
+        v = 0
+    elif algorithm == 'ridge':
+        v = np.identity(n)
+    elif algorithm == 'elastic':
+        v = (1 - rho) * np.identity(n)
+    else:
+        raise TypeError(f'Invalid algorithm type provided: {algorithm}')
+
+    PoisonLogger.info(f'Found identiy matrix.')
+
+    PoisonLogger.info('Trying to find sigma.')
+    sigma = sum([np.outer(i, i) for i in X]) / n
+    sigma_term = sigma + alph * v
+    PoisonLogger.info('Trying to find mu.')
+    mu = np.mean(X, axis=0)
+    PoisonLogger.info('Trying to find M.')
+    M = np.outer(ax, weights) + ((np.dot(weights, ax) + biases) - ay) * np.identity(n)
+
+    PoisonLogger.info('Concatenating matrices for final left matrix and right matrix.')
+    l_matrix = np.vstack((sigma_term, mu))
+    mu_append = np.append(mu, 1)
+    l_matrix = np.hstack((l_matrix, np.array([mu_append]).T))
+    r_matrix = np.vstack((M, weights)) * (-1/n)
+
+    PoisonLogger.debug(f'\nWeights:\n{weights}')
+    PoisonLogger.debug(f'\nM:\n{M}')
+    PoisonLogger.debug(f'\nl_matrix:\n{l_matrix}')
+    PoisonLogger.debug(f'\nr_matrix\n{r_matrix}')
+    
+    PoisonLogger.info('Checking left_matrix for singulartiy.')
+    if np.linalg.cond(l_matrix) < 1/float_info.epsilon:
+        PoisonLogger.info(f'Inverting left matrix.')
+        result = np.matmul(np.linalg.inv(l_matrix), r_matrix)
+        PoisonLogger.debug(f'\nresult inv:\n{result}')
+    else:
+        PoisonLogger.info(f'Left matrix is singular, trying pinv instead.')
+        result = np.matmul(np.linalg.pinv(l_matrix), r_matrix)
+        PoisonLogger.debug(f'\nresult pinv:\n{result}')
+
+    return result[:-1], result[-1]
+
+def equation4(X, Y, ax, ay, weights, biases, **kwargs):
+    
+    """Equation 4 in paper. Returns the partial derivatives of the weights and biases with respect to the attack point.
+
+    Parameters:
+        X: Data
+        Y: Results
+        ax: Attack point
+        ay: Attack point y value
+        weights: weights
+        biases: biases
+        Arguments:
+            type : (str) : type of algorithm to use ::: {'lasso', 'ridge', 'elastic'}
+                default => 'lasso'
+            alpha : (number) : alpha to be used in linear model by sklearn
+                default => 1.0
+            rho : (number) : rho used in sklearn for elastic net. Same as l1_ratio
+                default => 0.5
+                
+    Raises:
+        ValueError: Invalid X and Y dimensions
+        TypeError: Unknown parameters
+        TypeError: Invalid algorithm type
+
+    Returns:
+        Tuple: partial derivatives with respect to weights for (weights, biases)
+    """
+    
+    if kwargs:
+        PoisonLogger.info(f'Got arguments: {kwargs}')
+    
+    algorithm = kwargs.pop('type', 'lasso')
+    alph = kwargs.pop('alpha', 1.0)
+    rho = kwargs.pop('rho', 0.5)
+    
+    if kwargs:
+        raise TypeError('Unknown parameters: ' + ', '.join(kwargs.keys()))
+    
+    X = np.array(X)
+    Y = np.array(Y)
+    ax = np.array(ax)
+    weights = np.array(weights)
+    
+    if (X.shape[0] != Y.shape[0] or X.shape[1] != ax.shape[0] or X.shape[1] != weights.shape[0]):
+        raise ValueError('X and Y must have the same dimensions. ax and weights must have same dimensions. X second dimension must equal ax dimension.')
+    
+    partial_weights, partial_biases = equation7(X, Y, ax, ay, weights, biases, **kwargs)
+    last_term = alph * (np.matmul(r_term(weights, type=algorithm, rho=rho), partial_weights))
+    PoisonLogger.debug(f'\nlast_term:\n{last_term}')
+    
+    result = [((np.dot(item[0], ax) + biases) - item[1]) * (np.matmul(item[0], partial_weights) + partial_biases) for item in zip(X, Y)]
+    result = (sum(result) / X.shape[0]) + last_term
+    
+    PoisonLogger.debug(f'\nResult:\n{result}')
+    
+    return result
--- tests/Gradient_test.py
+++ tests/Gradient_test.py
@@ -1,5 +1,4 @@
-from poisoning.xiao2018.gradient import r_term, subgradient
-from poisoning.xiao2018.equations import equation7
+from poisoning.xiao2018.gradient import r_term, subgradient, equation7, equation4
 import numpy as np
 import pytest
 from tests.shared import find_inputs
@@ -96,3 +95,42 @@ class Test_Equation_7:
         weights = inputs[4]
         biases = inputs[5]
         res = equation7(X, Y, ax, ay, weights, biases, type=tp, alpha=const[0], rho=const[1])
+        
+class Test_Equation_4_fail:
+    okay_X = [[1, 2], [3, 4]]
+    okay_Y = [1, 2]
+    okay_ax = [3, 1]
+    okay_ay = 3.2
+    okay_weights = [0.2, 1.3]
+    okay_biases = 0.1
+    okay_lambda = 0.23
+    
+    @pytest.mark.parametrize('param', [{'this':3}, {'test123':332}, {'other':'haha'}, {'thing':'t'}])
+    def test_parameters(self, param):
+        with pytest.raises(TypeError) as info:
+            equation4(Test_Equation_4_fail.okay_X, Test_Equation_4_fail.okay_Y, Test_Equation_4_fail.okay_ax, Test_Equation_4_fail.okay_ay, Test_Equation_4_fail.okay_weights, Test_Equation_4_fail.okay_biases, **param)        
+
+    @pytest.mark.parametrize('tp', ['notlasso', 3, 92, 'other', 9.2, True])
+    def test_type(self, tp):
+        with pytest.raises(TypeError) as info:
+            equation4(Test_Equation_4_fail.okay_X, Test_Equation_4_fail.okay_Y, Test_Equation_4_fail.okay_ax, Test_Equation_4_fail.okay_ay, Test_Equation_4_fail.okay_weights, Test_Equation_4_fail.okay_biases, type=tp)
+    
+    @pytest.mark.parametrize('addx,addy,adda,addw', [([[3,3]],[],[],[]),([],[3],[],[]),([],[],[3],[]),([],[],[],[3])])
+    def test_dimensions(self, addx,addy,adda,addw):
+        
+        with pytest.raises(ValueError) as info:
+            equation4(Test_Equation_4_fail.okay_X + addx, Test_Equation_4_fail.okay_Y + addy, Test_Equation_4_fail.okay_ax + adda, Test_Equation_4_fail.okay_ay, Test_Equation_4_fail.okay_weights + addw, Test_Equation_4_fail.okay_biases)
+            
+class Test_Equation_4:
+    
+    @pytest.mark.parametrize('inputs', find_inputs('eq8'))
+    @pytest.mark.parametrize('tp', ['lasso', 'ridge', 'elastic'])
+    @pytest.mark.parametrize('const', [(0.7,0.7),(0.3,0.4)])
+    def test_inputs(self, tp, inputs, const):
+        X = inputs[0]
+        Y = inputs[1]
+        ax = inputs[2]
+        ay = inputs[3]
+        weights = inputs[4]
+        biases = inputs[5]
+        res = equation4(X, Y, ax, ay, weights, biases, type=tp, alpha=const[0], rho=const[1])
