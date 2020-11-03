6753f7f15ab88ade3a2873b10c57f545d59c9138
Roman Golota <rpgolota@gmail.com>
2020-11-02 19:47:39 -0700

    added dataset tests

--- tests/Input_test.py
+++ tests/Input_test.py
@@ -54,9 +54,29 @@ def test_project(proj_type, size):
 # @pytest.mark.xfail(reason='Not finished implementing.')
 @pytest.mark.parametrize('type', ALGORITHM_TYPE_SMALL)
 @pytest.mark.parametrize('file', find_inputs('Input_test'))
-def test_run(file, type):
+def test_manual(file, type):
     inp = read_json(file)
     test = xiao2018(type=type)
+    res = test.run(*inp)
+    print(res - np.array(inp[2]))
+    assert res.shape[0] == len(inp[2]) and res.shape[1] == len(inp[2][0])
+    
+@pytest.mark.parametrize('type', ALGORITHM_TYPE_SMALL)
+@pytest.mark.parametrize('file', find_inputs('Input_dataset'))
+def test_dataset(file, type):
+    inp = read_json(file)
+    test = xiao2018(type=type, max_iter=10)
+    res = test.run(*inp)
+    print(res - np.array(inp[2]))
+    assert res.shape[0] == len(inp[2]) and res.shape[1] == len(inp[2][0])
+    
+
+@pytest.mark.skip(reason='Too large to run.')
+@pytest.mark.parametrize('type', ALGORITHM_TYPE_SMALL)
+@pytest.mark.parametrize('file', find_inputs('Input_large_dataset'))
+def test_large_dataset(file, type):
+    inp = read_json(file)
+    test = xiao2018(type=type, max_iter=10)
     res = test.run(*inp)
     print(res - np.array(inp[2]))
     assert res.shape[0] == len(inp[2]) and res.shape[1] == len(inp[2][0])
