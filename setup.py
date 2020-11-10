699e732ae420df6de724fcbc3a86b6b1fefe1ed2
Roman Golota <rpgolota@gmail.com>
2020-11-09 21:35:44 -0700

    made setup.py manually adding packages

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setuptools.setup(
     name='poisoning',
     version='0.1',
     python_requires='>=3.7',
-    packages=setuptools.find_packages(),
+    packages=['poisoning', 'poisoning/xiao2018'],
     install_requires=['pytest>=5.4',
                       'numpy>=1.18',
                       'sklearn'                     
