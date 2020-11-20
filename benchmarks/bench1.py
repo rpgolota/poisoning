a3860a6f0c046d51ef5a8a289b1131a00afb09b7
Roman Golota <rpgolota@gmail.com>
2020-11-20 01:18:40 -0700

    addded benchmarks

--- benchmarks/bench1.py
+++ benchmarks/bench1.py
@@ -6,31 +6,35 @@ def read_json(filename):
     with open(filename, 'r') as f:
         return json.load(f)
 
-algorithms = {'fred': poisoning.frederickson2018}
+def main():
+    algorithms = {'xiao': poisoning.xiao2018, 'fred': poisoning.frederickson2018}
 
-files = {'files/Input_dataset_musk-1.json':'musk1',
-         'files/Input_dataset_musk-2.json':'musk2',
-         'files/Input_large_dataset_miniboone.json':'miniboone'}
+    files = {'files/Input_dataset_musk-1.json':'musk1',
+            'files/Input_dataset_musk-2.json':'musk2',
+            'files/Input_large_dataset_miniboone.json':'miniboone'}
 
-projections = [1]
-num_attacks = [1, 2, 3, 4, 5]
+    projections = [1]
+    num_attacks = [1, 2, 3, 4, 5, 10]
 
-for algorithm in algorithms:
-    for file in files:
-        for projection in projections:
-            for attack in num_attacks:
-                X, Y, _, _ = read_json(file)
-                with pBench() as b:
-                    A = algorithms[algorithm](max_iter=1)
-                    A.autorun(X, Y, attack, projection)
+    for algorithm in algorithms:
+        for file in files:
+            for projection in projections:
+                for attack in num_attacks:
+                    X, Y, _, _ = read_json(file)
+                    with pBench() as b:
+                        A = algorithms[algorithm](max_iter=1)
+                        A.autorun(X, Y, attack, projection)
 
-                sample_size = len(Y)
-                feature_size = len(X[0])
-                
-                text = f'File: {file}\nSamples: {sample_size}   Features: {feature_size}\n\n'
-                filename = f'{files[file]}_A{attack}_P{projection}.txt'
-                
-                with open(f'{algorithm}/' + filename, 'w') as f:
-                    f.write(text)
+                    sample_size = len(Y)
+                    feature_size = len(X[0])
                     
-                b.write(f'{algorithm}/' + filename, flag='a')
+                    text = f'File: {file}\nSamples: {sample_size}   Features: {feature_size}\n\n'
+                    filename = f'{files[file]}_A{attack}_P{projection}.txt'
+                    
+                    with open(f'{algorithm}/' + filename, 'w') as f:
+                        f.write(text)
+                        
+                    b.write(f'{algorithm}/' + filename, flag='a')
+                
+if __name__ == "__main__":
+    main()
