54c42c57c4f1e7c82a0d522be2f8f773ee475b55
Roman Golota <rpgolota@gmail.com>
2020-12-08 02:15:10 -0700

    added versioning to project

--- setup.py
+++ setup.py
@@ -1,15 +1,37 @@
 import setuptools
 
+def get_version():
+    exec(open('poisoning/version.py').read(), locals())
+    return locals()['__version__']
+
+name = 'poisoning'
+
+version = get_version()
+
+description = 'Poisoning datasets using gradient ascent, targeting feature selection.'
+
+url='https://github.com/rpgolota/poisoning/'
+
+python_requires = '>=3.7'
+
+packages = ['poisoning']
+
+install_requires = ['pytest>=5.4',
+                    'numpy>=1.18,<=1.19.3',
+                    'sklearn']
+
+classifiers = [ 'Development Status :: 4 - Beta',
+                'Intended Audience :: Science/Research',
+                'Operating System :: OS Independent',
+                'Programming Language :: Python :: 3.8']
+
 setuptools.setup(
-    name='poisoning',
-    version='0.1',
-    python_requires='>=3.7',
-    packages=['poisoning'],
-    install_requires=['pytest>=5.4',
-                      'numpy>=1.18,<=1.19.3',
-                      'sklearn'
-                      ],
-    classifiers=[    
-        'Programming Language :: Python :: 3.8',
-    ],
+    name=name,
+    version=version,
+    description=description,
+    url=url,
+    python_requires=python_requires,
+    packages=packages,
+    install_requires=install_requires,
+    classifiers=classifiers,
 )
