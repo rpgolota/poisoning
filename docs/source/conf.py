dfc76215cb524d0399d9eefe023f267df5ae5b8d
Roman Golota <rpgolota@gmail.com>
2020-12-08 02:11:58 -0700

    updated configuration for sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,19 +10,24 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../..'))
-
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
 
 # -- Project information -----------------------------------------------------
 
+def get_version():
+    exec(open('../../poisoning/version.py').read(), locals())
+    return locals()['__version__']
+
 project = 'poisoning'
+
 copyright = '2020, Roman Golota, Eduardo Zambrana, Srishti Gupta'
+
 author = 'Roman Golota, Eduardo Zambrana, Srishti Gupta'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = get_version()
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,7 +35,10 @@ release = '0.0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'numpydoc']
+extensions = [
+    'sphinx.ext.autodoc',
+    'numpydoc'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
